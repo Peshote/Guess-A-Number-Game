c1fdf444f28b20eb592543218aea0c1b2bd6e3b7
Petar Iliev <peshote@gmail.com>
2023-08-06 01:37:15 +0300

    Update Guess-A-Number.cs
    Add a comments to explain the algorithm logic

--- Project-Guess-A-Number/Guess-A-Number.cs
+++ Project-Guess-A-Number/Guess-A-Number.cs
@@ -10,27 +10,27 @@ namespace Project_Guess_A_Number
 
         static void Main(string[] args)
         {
-            PlayGame();
+            PlayGame(); // Start the game
         }
 
         static void PlayGame()
         {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
+            Stopwatch timer = new Stopwatch(); // Create a stopwatch to measure time
+            timer.Start(); // Start the stopwatch
 
             if (currentLevel <= 5)
             {
                 int minRange = 1;
-                int maxRange = GetMaxRangeForLevel(currentLevel);
+                int maxRange = GetMaxRangeForLevel(currentLevel); // Determine the maximum range for the current level
 
                 Random randomNumber = new Random();
-                int computerNumber = randomNumber.Next(minRange, maxRange + 1);
+                int computerNumber = randomNumber.Next(minRange, maxRange + 1); // Generate a random number for the current level
 
                 while (true)
                 {
                     Console.Write("Level ");
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write(currentLevel);
+                    Console.Write(currentLevel); // Display the current level in cyan color
                     Console.ResetColor();
                     Console.Write($" - Guess a number ({minRange}-{maxRange}): ");
 
@@ -46,7 +46,7 @@ namespace Project_Guess_A_Number
                             Console.ForegroundColor = ConsoleColor.Green;
                             Console.WriteLine($"Congratulations! You've completed level {currentLevel}.");
                             Console.ResetColor();
-                            currentLevel++;
+                            currentLevel++; // Move to the next level
                             Console.WriteLine();
                             break;
                         }
@@ -71,30 +71,31 @@ namespace Project_Guess_A_Number
 
                 if (currentLevel <= 5)
                 {
-                    PlayGame();
+                    PlayGame(); // Play the next level
                 }
                 else
                 {
-                    timer.Stop();
-                    TimeSpan totalTime = timer.Elapsed;
+                    timer.Stop(); // Stop the stopwatch
+                    TimeSpan totalTime = timer.Elapsed; // Calculate the total time taken
 
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine($"Congratulations! You've completed all levels in {totalTime:hh\\:mm\\:ss} time.");
 
                     if (totalTime < bestTime)
                     {
-                        bestTime = totalTime;
+                        bestTime = totalTime; // Update the best time record if needed
                         Console.WriteLine($"Your best time record is {bestTime:hh\\:mm\\:ss} !");
                     }
 
                     Console.ResetColor();
-                    PlayAgain();
+                    PlayAgain(); // Ask if the player wants to play again
                 }
             }
         }
 
         static int GetMaxRangeForLevel(int level)
         {
+            // Determine the maximum range for each level
             switch (level)
             {
                 case 1:
@@ -128,7 +129,7 @@ namespace Project_Guess_A_Number
             {
                 currentLevel = 1;
                 Console.WriteLine();
-                PlayGame();
+                PlayGame(); // Restart the game
             }
             else
             {
